f6df57fa467f252c7a86d697fd54a66a35a9faa7
Zach Roth <zacharywroth@gmail.com>
2022-10-27 11:50:00 +0200

    Add high-pass filter, low-pass filter, and rectification to Filtering Module

--- synergies-1/synergies_individual_walking.m
+++ synergies-1/synergies_individual_walking.m
@@ -20,7 +20,7 @@ tic
 %--------------------------------------------------------------------------
 
 % 1. Import Data
-% point to the folder containing the EMG data
+% Point to the folder containing the EMG data
 emg_dir = uigetdir();
 
 tic
@@ -32,14 +32,70 @@ emg_files = dir(fullfile(emg_dir, '*.xlsx'));
 emg_files = {emg_files.name}';
 
 % Collate the EMG files into a single structure
-for ii = 1:length(emg_files)
-    trial_num = sscanf(emg_files{ii},strcat("EMG_raw_","%f",".xlsx"));
-    data = readtable(emg_files{ii});
+for file = 1:length(emg_files)
+    trial_num = sscanf(emg_files{file},strcat("EMG_raw_","%f",".xlsx"));
+    data = readtable(emg_files{file});
     if isempty(data) == 1
+       disp(['File ',num2str(file),' empty'])
         continue
     else
         emg_data.(['trial' num2str(trial_num)]) = data;
+        disp(['File ',num2str(file),' imported'])
     end
 end
+
+% 2. Filtering
+
+% Create a HIGH-pass 4-order Butterworth Filter
+fc = 10; % cutoff frequency
+fs = 1000; % sampling rate
+n  = 4; % nth-order
+Wn = fc/(fs/2); % Cut off frequency
+ftype = 'high'; % Filter type
+
+[b,a] = butter(n,Wn,ftype);
+
+% HIGH-pass filter EMG data
+trials = fieldnames(emg_data);
+for trial = 1:length(trials)
+    trial_num = sscanf(trials{trial},strcat("trial","%f"));
+    for muscle = 2:17
+        x = table2array(emg_data.(['trial' num2str(trial_num)])(:,muscle));
+        emg_data.(['trial' num2str(trial_num)])(:,muscle) = num2cell(filtfilt(b,a,x));
+    end
+    disp(['Trial',num2str(trial),' high-pass filtered'])
+end
+
+% Create a LOW-pass 4-order Butterworth Filter
+fc = 50; % cutoff frequency
+fs = 1000; % sampling rate
+n  = 4; % nth-order
+Wn = fc/(fs/2); % Cut off frequency
+ftype = 'low'; % Filter type
+
+[b,a] = butter(n,Wn,ftype);
+
+% LOW-pass filter EMG data
+trials = fieldnames(emg_data);
+for trial = 1:length(trials)
+    trial_num = sscanf(trials{trial},strcat("trial","%f"));
+    for muscle = 2:17
+        x = table2array(emg_data.(['trial' num2str(trial_num)])(:,muscle));
+        emg_data.(['trial' num2str(trial_num)])(:,muscle) = num2cell(filtfilt(b,a,x));
+    end
+    disp(['Trial',num2str(trial_num),' low-pass filtered'])
+end
+
+% Rectify signal
+for trial = 1:length(trials)
+    trial_num = sscanf(trials{trial},strcat("trial","%f"));
+    for muscle = 2:17
+        filt_emg = table2array(emg_data.(['trial' num2str(trial_num)])(:,muscle));
+        emg_data.(['trial' num2str(trial_num)])(:,muscle) = num2cell(abs(filt_emg));
+    end
+    disp(['Trial',num2str(trial_num),' rectified'])
+end
+
+
 
 toc
